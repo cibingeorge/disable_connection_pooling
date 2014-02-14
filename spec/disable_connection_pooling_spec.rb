d50c069a2e0a0801005823859542724d8e7251ed
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-02-14 22:57:48 +0900

    Fix syntax error

--- spec/disable_connection_pooling_spec.rb
+++ spec/disable_connection_pooling_spec.rb
@@ -1,10 +1,17 @@
-describe 'disable_connection_pooling' do
-  describe 'when Rails 3.2 / disable_connection_pooling = false'
+describe 'Rails 3.2' do
+  describe 'when disable_connection_pooling = false' do
     it 'should have been connected after request' do
       send_request(3200)
-
       threads = processlist('rails32_dummy')
       expect(threads.length).to be >= 1
+    end
+  end
+
+  describe 'when disable_connection_pooling = true' do
+    it 'should have been disconnected after request' do
+      send_request(3201)
+      threads = processlist('rails32_dummy')
+      expect(threads.length).to eq(0)
     end
   end
 end
