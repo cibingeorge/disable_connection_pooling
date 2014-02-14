55b30a927b85844ad42357e91fba17187a218425
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-02-15 00:06:41 +0900

    Add spec

--- spec/disable_connection_pooling_spec.rb
+++ spec/disable_connection_pooling_spec.rb
@@ -15,20 +15,6 @@ describe 'disable_connection_pooling' do
         expect(threads.length).to eq(0)
       end
     end
-
-    describe 'when number_of_requests_to_disconnecting = 3' do
-      it 'should have been connected after request' do
-        send_request(3202)
-        threads = processlist('rails32_dummy')
-        expect(threads.length).to be >= 1
-      end
-
-      it 'should have been disconnected after request' do
-        2.times { send_request(3202) }
-        threads = processlist('rails32_dummy')
-        expect(threads.length).to eq(0)
-      end
-    end
   end
 
   describe 'when Rails 4.0' do
@@ -43,20 +29,6 @@ describe 'disable_connection_pooling' do
     describe 'when disable_connection_pooling = true' do
       it 'should have been disconnected after request' do
         send_request(4001)
-        threads = processlist('rails40_dummy')
-        expect(threads.length).to eq(0)
-      end
-    end
-
-    describe 'when number_of_requests_to_disconnecting = 3' do
-      it 'should have been connected after request' do
-        send_request(4002)
-        threads = processlist('rails40_dummy')
-        expect(threads.length).to be >= 1
-      end
-
-      it 'should have been disconnected after request' do
-        2.times { send_request(4002) }
         threads = processlist('rails40_dummy')
         expect(threads.length).to eq(0)
       end
