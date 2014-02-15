5f4b60c8e8de926ff408e10c06ce716949b11b1d
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-02-15 09:44:35 +0900

    Add spec

--- spec/disable_connection_pooling_spec.rb
+++ spec/disable_connection_pooling_spec.rb
@@ -51,6 +51,25 @@ describe 'disable_connection_pooling' do
         threads = processlist('rails40_dummy')
         expect(threads.length).to eq(0)
       end
+
+      it 'should have been connected until end of the request' do
+        script = <<-EOS
+          (1..3).map do
+            sleep 1
+            Item.find_by_sql('SHOW PROCESSLIST').to_a
+          end
+        EOS
+
+        json = run_script(4001, script)
+        threads_list =  JSON.parse(json)
+
+        id_list = threads_list.map {|threads|
+          threads.select {|i| i['db'] == 'rails40_dummy' }.map {|i| i['Id'] }
+        }.flatten
+
+        expect(id_list.length).to eq(3)
+        expect(id_list.uniq.length).to eq(1)
+      end
     end
   end
 end
